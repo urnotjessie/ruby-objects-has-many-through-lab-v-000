72f3cbb9bc1f98822d2cea63e005bc0f0d1512ef
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-07 23:49:45 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -10,4 +10,8 @@ class Patient
   def add_appointment(appointment)
     @appointments << appointment
   end
+
+  def doctors
+    self.appointments.collect do |appointment|
+      appointment.doctor
 end
